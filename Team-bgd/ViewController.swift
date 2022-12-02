00749ca075db97b4975ded4676bd28701aac4d3f
baegteun <baegteun@gmail.com>
2022-12-03 00:55:12 +0900

    :tada: :: Begin

---
+++ Team-bgd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Team-bgd
+//
+//  Created by 최형우 on 2022/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
